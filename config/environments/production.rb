cdcdffb17b56a584ade129e0dd774354ff256d2f
Chris Spirito <chris.spirito@gmail.com>
2014-01-29 17:54:56 +0200

    Ember updates to config/environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,9 @@ FlexiconEmber::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # EMBER
+  config.ember.variant = :production
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,6 +19,9 @@ FlexiconEmber::Application.configure do
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
 
+  # Ember
+  config.ember.variant = :development
+
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
 
